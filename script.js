65e9b16dd108352d2e68353f795e94474e05d95b
sofiaesther <99698605+sofiaesther@users.noreply.github.com>
2022-06-25 21:35:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -10,22 +10,17 @@ requireName.then(nameSucess);
 requireName.catch(nameError);
 
 function nameError(){
-    user = prompt("Nome em uso, insira outro user");
-    username ={
-        name: String(user)
-      };
-    requireName = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', username);
+    window.location.reload()
 }
 function nameSucess(){
     console.log(username);
 }
 const statusVerify = setInterval( function (){const requireStatus = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', username);}, 5000)
 
-let messagesdata =[];
-const messagesPromisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-messagesPromisse.then(messageSucess);
+const atualizemessages = setInterval(reloadMessages,5000)
 
-messagesPromisse.catch(messageError);
+let messagesdata =[];
+reloadMessages();
 
 function messageError(){
     console.log('erro')
@@ -35,9 +30,9 @@ function messageSucess(message){
     console.log(messagesdata);
     renderMessages();
 }
-let messageitem;
 function renderMessages(){
     for (let i=0; i<messagesdata.length ;i++){
+        let messageitem;
         if (messagesdata[i].type=='status'){
             messageitem = `
             <li>
@@ -46,11 +41,11 @@ function renderMessages(){
             </div>
             </li>
         `;
-        } else{
+        } else if (messagesdata[i].type=='message'||messagesdata[i].to==user){
             messageitem = `
             <li>
             <div class="${messagesdata[i].type}"> 
-               <h3>(${messagesdata[i].time}) </h3> <h4>-</h4><h1> ${messagesdata[i].from} </h1> <h4>-</h4><p>  para  </p><h4>-</h4><h1> ${messagesdata[i].to} </h1><p>: ${messagesdata[i].text}</p>
+               <h3>(${messagesdata[i].time})</h3><h4>-</h4><h1>${messagesdata[i].from} </h1> <h4>-</h4><p>  para  </p><h4>-</h4><h1> ${messagesdata[i].to} </h1><p>: ${messagesdata[i].text}</p>
             </div>
             </li>
         `;
@@ -60,4 +55,40 @@ function renderMessages(){
     }
     const lastmessage = document.querySelector('ul li:last-child');
     lastmessage.scrollIntoView();
+}
+let messagecontent;
+function sendMessage(){
+    const messageText = document.querySelector("input").value;
+    messagecontent ={
+        from: String(user),
+        to: "Todos",
+        text: String(messageText),
+        type: "message"
+    }
+    document.querySelector("input").placeholder = "Escreva aqui...";
+    document.querySelector("input").value ='';
+const requireMessage = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messagecontent)
+
+requireMessage.then(sendmessageSucess);
+
+requireMessage.catch(sendmessageError);
+}
+function sendmessageError(){
+    console.log('erro')
+    window.location.reload()
+}
+function sendmessageSucess(){
+    console.log(messagecontent);
+    let messageConfirmed = document.querySelector(".send").innerHTML;
+    let confirmation = `
+    <p>Mensagem enviada com sucesso para ${messagecontent.to}</p>
+    `;
+    messageConfirmed += confirmation;
+    reloadMessages();
+}
+
+function reloadMessages(){
+    const messagesPromisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    messagesPromisse.then(messageSucess);
+    messagesPromisse.catch(messageError);
 }
